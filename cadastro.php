dca82164d0b3aad61b20be8ad1756244152a8fba
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2015-11-26 11:13:27 -0200

    Ajustes no formulario

--- cadastro.php
+++ cadastro.php
@@ -11,32 +11,40 @@
 include("cabecalho.php");
 ?>
 
-
-	<h1>Cadastro</h1>
-	<main>
-	<div class="row">
-		<form class="col s6" method="post">
-			<div class="row">
-	        	<div class="input-field col s12">
-					<label>Nome completo</label>
-					<input type="text" name="nome" />
+	<div class="container">
+		<h1 class="header">Cadastro</h1>
+		<main>
+		<div class="row">
+			<form class="col s6" method="post">
+				<div class="row">
+		        	<div class="input-field col s12">
+						<label>Nome completo</label>
+						<input type="text" name="nome" />
+					</div>
+					<div class="input-field col s12">
+						<label>E-mail</label>
+						<input type="text" name="email" />
+					</div>
+					<div class="input-field col s12">
+						<label>Senha</label>
+						<input type="password" name="senha" />
+					</div>
+					<div class="input-field col s12">
+					      <input type="checkbox" id="news" name="news" />
+					      <label for="news">Deseja se inscrever em nosso newsletter?</label>
+    				</div>
+					
+					<div class="input-field col s12 sub">
+						<button class="waves-effect waves-light btn blue" type="button"><i class="material-icons left">save</i>Salvar</button>
+					</div>
+					
 				</div>
-				<div class="input-field col s12">
-					<label>E-mail</label>
-					<input type="text" name="email" />
-				</div>
-				<div class="input-field col s12">
-					<label>Senha</label>
-					<input type="password" name="senha" />
-				</div>
-				<div class="input-field col s12">
-					<button class="waves-effect waves-light btn blue" type="button"><i class="material-icons left">save</i>Salvar</button>
-				</div>
-				
-			</div>
-		</form>
+			</form>
+		</div>
+		</main>
 	</div>
-	</main>
-<?php 
-	include("rodape.php");
-?>
+	<div class="fixo">
+		<?php 
+			include("rodape.php");
+		?>
+	</div>
